6678a70844ab3ec28829f6de84b9544d420f39e0
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2023-01-27 21:54:52 +1100

    Patch.gwt.xml

---
+++ src/it/gwt-jar-test/src/test/java/test/TestGwtTest.java
@@ -0,0 +1,35 @@
+package test;
+
+import com.google.gwt.junit.client.GWTTestCase;
+
+import walkingkooka.tree.json.JsonNode;
+import walkingkooka.tree.json.patch.Patchable;
+import walkingkooka.tree.json.marshall.JsonNodeUnmarshallContext;
+
+public class TestGwtTest extends GWTTestCase {
+
+    @Override
+    public String getModuleName() {
+        return "test.Test";
+    }
+
+    public void testAssertEquals() {
+        assertEquals(
+                1,
+                1
+        );
+    }
+
+    public void testParse() {
+        new TestPatchable();
+    }
+
+    final static class TestPatchable implements Patchable<TestPatchable> {
+
+        @Override
+        public TestPatchable patch(final JsonNode json,
+                                   final JsonNodeUnmarshallContext context) {
+            throw new UnsupportedOperationException();
+        }
+    }
+}
